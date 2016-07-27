37e0caf6bb43db4d51ad3e9886fb23ae51fc00a9
Mauricio <Ramos@Mauricios-MBP.home>
2016-07-26 20:53:48 -0400

    burger

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -16,7 +16,7 @@ module.exports = function(app){
 
 
 	app.get('/', function(req, res){
-		res.sendFile(path.join(__dirname + '/../public/auth.html'));
+		res.sendFile(path.join(__dirname + '/../public/burger.html'));
 	
 
 	});
